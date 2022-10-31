f68c4cef0f35f6dfde55968a7ac07dcfb6822ca4
Chris Duvivier <cduvivie@student.s19.be>
2022-10-31 17:33:02 +0100

    wip on predict.py. TODO: add index to prediction exported. create clean_testset from raw dataset_test at the beginning of predict.py.  Add dataset_test as parameter to predict.py.

--- logreg_predict.py
+++ logreg_predict.py
@@ -7,15 +7,27 @@ from LR_models import LogisticRegression, MultipleLogisticRegression
 import data.data_tools as dt
 import json
 
-
 def main(sys_argv):
+    df_test = pd.read_csv("datasets/clean/cleaned_test.csv", index_col = False)
+    
+    target_columns = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
+    X_test = np.array(df_test)
+
     Models = MultipleLogisticRegression()
-    data = Data(datafile="datasets/dataset_test.csv")
-    with open("weights.json", "r") as outfile:
-        weights = json.load(outfile)
-    print(weights)
-    df = pd.read_csv("datasets/cleaned_test.csv")
-    data.df = dt.clean_dataset(data)
+    Models.load_weights("Model_parameters.json")    # load saved weights 
+    predictions = Models.predict(X_test)
+
+    # Export predictions (numpy array) to csv with name of houses
+    df_export = pd.DataFrame(predictions, columns=['Hogwarts House'])
+    for i in range(len(df_export)):
+        idx = df_export['Hogwarts House'][i]
+        df_export['Hogwarts House'][i] = target_columns[idx]
+
+    print(df_export.head(10))
+
+    # Save to csv
+    df_export.to_csv('prediction.csv', index=False)
+
 
 if __name__ == '__main__':
     main(sys.argv)
